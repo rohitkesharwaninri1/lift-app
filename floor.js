08bf74987b3e396310e4ec57f947b52936084518
Rohit kesharwani <rohitkesharwaninri1@gmail.com>
2021-10-23 12:31:55 +0530

    Initialize project using Create React App

---
+++ floor.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function floor() {
+    return (
+        <div style={{backgroundColor:'#dadada', width:'100%', height:'200px',padding:'20px'}}>
+            <button>Up</button><br/>
+            <button>down</button>
+        </div>
+    )
+}
